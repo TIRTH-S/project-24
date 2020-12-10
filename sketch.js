767231c333dad3bb812bd858ee825115fe9c93e8
TIRTH-S <72561689+TIRTH-S@users.noreply.github.com>
2020-12-10 22:43:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	ground= new Ground(700,600,1200,20);
+
+ trash=new ball(200,500);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  ground.display();
+  
+  drawSprites();
+ 
+}
+
+
+
